838f5fd3b3b69367ed49e226d0f92e57796d0323
LET <bjs.6300.137@gmail.com>
2024-07-24 10:27:35 +0900

    Initial Commit

---
+++ PostalCodeSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PostalCodeSearch
+//
+//  Created by 津本拓也 on 2024/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PostalCodeSearch/PostalCodeSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostalCodeSearchApp.swift
+//  PostalCodeSearch
+//
+//  Created by 津本拓也 on 2024/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PostalCodeSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
